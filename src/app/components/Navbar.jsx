28cef90d0fbc3e0d3b53c9df5129d42bfe9b3da7
divyang1507 <divhp1507@gmail.com>
2024-06-13 08:28:38 +0530

    Data.js added

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,24 +3,28 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header class="text-black body-font w-[90%] mx-auto bg-orange-200 rounded-full mt-5">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="flex title-font font-medium items-center text-black mb-4 md:mb-0 hover:cursor-pointer">
+    <header className="text-black body-font w-[90%] mx-auto bg-orange-200 rounded-full mt-5">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-black mb-4 md:mb-0 hover:cursor-pointer">
           <Image
             src="/LogoImage.png"
             width={56}
             height={56}
             alt="OrangeBookPublicationLogo"
           />
-          <span class="ml-3 text-xl font-semibold">
+          <span className="ml-3 text-xl font-semibold">
             Orange Book <br /> Publication
           </span>
         </a>
-        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a class="mr-5 hover:text-gray-600 hover:cursor-pointer">Home</a>
-          <a class="mr-5 hover:text-gray-600 hover:cursor-pointer">Products</a>
-          <a class="mr-5 hover:text-gray-600 hover:cursor-pointer">Contact</a>
-          <a class="mr-5 hover:text-gray-600 hover:cursor-pointer">About</a>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-600 hover:cursor-pointer">Home</a>
+          <a className="mr-5 hover:text-gray-600 hover:cursor-pointer">
+            Products
+          </a>
+          <a className="mr-5 hover:text-gray-600 hover:cursor-pointer">
+            Contact
+          </a>
+          <a className="mr-5 hover:text-gray-600 hover:cursor-pointer">About</a>
         </nav>
       </div>
     </header>
---
+++ src/app/data.js
@@ -0,0 +1,19 @@
+export const bookdata = [
+  {
+    id: 1,
+    bookname: "Gujarati and English Letter writing",
+    Description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque earum animi vitae voluptatibus quia ullam, natus excepturi reiciendis et fugit recusandae perferendis esse quos exercitationem.",
+    rating: 4.5,
+    reviews: 1526,
+  },
+  {
+    id: 2,
+    bookname: "King Kong The Master Desi Hisab",
+    Description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque earum animi vitae voluptatibus quia ullam, natus excepturi reiciendis et fugit recusandae perferendis esse quos exercitationem.",
+    rating: 4.5,
+    reviews: 1526,
+  },
+];
+
