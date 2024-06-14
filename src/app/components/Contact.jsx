b3f301e35028514636271e10920dc6a9694b4d0e
divyang1507 <divhp1507@gmail.com>
2024-06-14 08:49:11 +0530

    contact form required

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -33,10 +33,11 @@ const Contact = () => {
           </div>
           <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-              Feedback
+              Contact Us
             </h2>
             <p className="leading-relaxed mb-5 text-gray-600">
-              Post-ironic portland shabby chic echo park, banjo fashion axe
+              Have any query regarding Our Books ? Keep in touch with Contacting
+              us
             </p>
             <div className="relative mb-4">
               <label htmlFor="name" className="leading-7 text-sm text-gray-600">
@@ -46,6 +47,7 @@ const Contact = () => {
                 type="text"
                 id="name"
                 name="name"
+                required
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
@@ -60,6 +62,7 @@ const Contact = () => {
                 type="email"
                 id="email"
                 name="email"
+                required
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
@@ -73,6 +76,7 @@ const Contact = () => {
               <textarea
                 id="message"
                 name="message"
+                required
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               ></textarea>
             </div>
